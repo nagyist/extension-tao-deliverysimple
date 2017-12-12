7df52361f84bae7cbf1c0f806b6401067c85b5d2
Ionut Padurariu <ionut@taotesting.com>
2017-12-12 10:07:22 +0100

    increase version

--- manifest.php
+++ manifest.php
@@ -27,7 +27,7 @@ return array(
 	'label' => 'Simple delivery model',
 	'description' => 'Single test delivery model',
     'license' => 'GPL-2.0',
-    'version' => '3.0.2',
+    'version' => '3.1.0',
 	'author' => 'Open Assessment Technologies, CRP Henri Tudor',
 	'requires' => array(
 	   'taoDeliveryTemplate' => '1.0'
